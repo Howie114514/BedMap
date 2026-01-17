c7798bd55ac3c4771e592c6abda20121b99ea78f
Howie114514 <qiuchenghao666@outlook.com>
2026-01-17 08:57:44 +0800

    移除服务器目标类型

--- xmake.lua
+++ xmake.lua
@@ -2,14 +2,7 @@ add_rules("mode.debug", "mode.release")
 
 add_repositories("levimc-repo https://github.com/LiteLDev/xmake-repo.git")
 
--- add_requires("levilamina x.x.x") for a specific version
--- add_requires("levilamina develop") to use develop version
--- please note that you should add bdslibrary yourself if using dev version
-if is_config("target_type", "server") then
-    add_requires("levilamina", {configs = {target_type = "server"}})
-else
-    add_requires("levilamina", {configs = {target_type = "client"}})
-end
+add_requires("levilamina 1.8.0-rc.1", {configs = {target_type = "client"}})
 
 add_requires("levibuildscript")
 
@@ -17,13 +10,7 @@ if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
 
-option("target_type")
-    set_default("server")
-    set_showmenu(true)
-    set_values("server", "client")
-option_end()
-
-target("my-mod") -- Change this to your mod name.
+target("bed-map") -- Change this to your mod name.
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
     add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
@@ -36,13 +23,5 @@ target("my-mod") -- Change this to your mod name.
     add_headerfiles("src/**.h")
     add_files("src/**.cpp")
     add_includedirs("src")
-    if is_config("target_type", "server") then
-        add_defines("LL_PLAT_S")
-    --  add_includedirs("src-server")
-    --  add_files("src-server/**.cpp")
-    else
-        add_defines("LL_PLAT_C")
-    --  add_includedirs("src-client")
-    --  add_files("src-client/**.cpp")
-    end
+    add_defines("LL_PLAT_C")
 
